174e7d84921d54e2a179087ca4b87b2f754cefc0
Matt Watson <mwatson@renci.org>
2026-02-19 08:31:30 -0500

    install, setup @astro/sitemap
    install astro sitemap
    
    setup astro sitemap
    
    wip
    
    correct url, install/update deps

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,15 +1,18 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import favicons from 'astro-favicons';
+import sitemap from '@astrojs/sitemap';
 
 const rootDir = dirname(fileURLToPath(import.meta.url));
 const uswdsPackages = join(rootDir, '../../node_modules/@uswds/uswds/packages');
 
 export default defineConfig({
-  integrations: [mdx(), react(), favicons()],
+  site: 'https://biodatacatalyst.nhlbi.nih.gov',
+  integrations: [mdx(), react(), sitemap()],
   markdown: {
     remarkPlugins: [['remark-excerpt', { remove: true }]],
   },
