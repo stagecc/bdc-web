f42d17234f5c95d7cb72d15655b7e51ee1898657
Matt Watson <mwatson@renci.org>
2026-02-23 16:16:03 -0500

    lint

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -2,9 +2,9 @@ import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import favicons from 'astro-favicons';
-import sitemap from '@astrojs/sitemap';
 
 const siteUrl = process.env.SITE_URL || 'https://biodatacatalyst.nhlbi.nih.gov';
 
