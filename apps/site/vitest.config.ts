7e5a28197bee1d00f45ed82413bcc6b01608bb39
Matt Watson <mwatson@renci.org>
2026-02-26 13:30:08 -0500

    move tests to project root, add docs site test

--- apps/site/vitest.config.ts
+++ apps/site/vitest.config.ts
@@ -1,8 +1,8 @@
 import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import { defineProject } from 'vitest/config';
 
-export default defineConfig({
+export default defineProject({
   plugins: [react()],
   resolve: {
     alias: {
