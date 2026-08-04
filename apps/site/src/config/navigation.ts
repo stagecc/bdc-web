c461148b93127806643d1ee22cecbca405f86f66
suejinkim20 <kim.suejin.20@gmail.com>
2026-08-04 14:31:11 -0400

    add 'BDC for learning and training' page to navigation and create corresponding content page

--- apps/site/src/config/navigation.ts
+++ apps/site/src/config/navigation.ts
@@ -22,6 +22,7 @@ export const navConfig: NavItem[] = [
       { label: 'Impute', href: '/data/impute' },
       { label: 'Analyze', href: '/data/analyze' },
       { label: 'Bring Your Own Tools', href: '/data/bring-your-own-tools' },
+      { label: 'BDC for Learning and Training', href: '/data/learning-and-training' },
     ],
   },
   {
