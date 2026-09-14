354cda04b5bb51bf566d102061e8578444ffd3d5
suejinkim20 <kim.suejin.20@gmail.com>
2026-09-14 12:25:40 -0400

    remove showOnContentPage field

--- apps/site/src/content.config.ts
+++ apps/site/src/content.config.ts
@@ -214,7 +214,6 @@ const programContent = defineCollection({
     title: z.string().optional(),
     priority: z.number().optional(),
     dataAvailable: z.boolean().default(true),
-    showOnContentPage: z.boolean().default(true),
   }),
 });
 
